ecc92bbc8615ac317f9bb2d67086b4011c477f81
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-02-23 10:15:19 -0700

    add sessions controller and login view

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(user_name: params[:user_name])
+    if @user and @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      flash.now[:alert] = 'Username and password do not match.'
+      render :new
+    end
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(user_name: 'mlouie', password: 'password', about: 'gSchool Student')
