816f7e08a71e8dc0da6d1b9f6d161a808a489d4d
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-02-23 11:34:37 -0700

    add new/create action and view for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,30 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
+
   def index
     @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      flash[:notice] = "Course successfully created."
+      redirect_to course_path(@course)
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :day_night)
   end
 end
