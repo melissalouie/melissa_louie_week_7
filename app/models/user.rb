fa73d139f325cdf2ac9b3807dbcde82766e30415
Melissa Louie <melissalouie@Melissas-MacBook-Pro.local>
2015-02-23 10:35:46 -0700

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :user_name, presence: true
+  validates :user_name, uniqueness: true
+  validates :about, presence: true
+  validates :password, presence: true
 end
